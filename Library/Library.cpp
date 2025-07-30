73b1dd0c00e71142f08efaeb8920de6af1cdc802
Lindsay <kuilagi.lindsay@gmail.com>
2025-07-30 18:18:55 +1000

    Add 2 new books

--- Library/Library.cpp
+++ Library/Library.cpp
@@ -8,18 +8,24 @@ int main() {
 	book2.setBookDetails("Harry Poter", "J.K Rowling", "DAM1703", false);
 	Book book3;
 	book3.setBookDetails("The Summer I Turned Pretty", "Jenny Han", "MAM2301", true);
+	Book book4;
+	book4.setBookDetails("A Game Of Thrones", "George.R Martin", "PAP0408", true);
+	Book book5;
+	book5.setBookDetails("The Lord Of The Rings", " J R R Tolkien", "ZIA1102", false);
 
-	Book myArray[3];
+	Book myArray[5];
 	myArray[0] = book1;
 	myArray[1] = book2;
 	myArray[2] = book3;
+	myArray[3] = book4;
+	myArray[4] = book5;
 
 	string isbnToBorrow = "";
 	while (isbnToBorrow != "0") {
 		cout << "Input the book's ISBN you want to borrow: ";
 		cin >> isbnToBorrow;
 
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 5; i++) {
 
 			if (myArray[i].isbn == isbnToBorrow) {
 				if (myArray[i].borrowBook()) {
