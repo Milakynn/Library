08360eb2436d10c7a550f5b430ca35a5e1707757
Lindsay <kuilagi.lindsay@gmail.com>
2025-08-06 19:37:54 +1000

    Add new book methods + sort book

--- Library/Book.h
+++ Library/Book.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 using namespace std;
+#include <string>
 
 class Book {
 public:
--- Library/Library.cpp
+++ Library/Library.cpp
@@ -2,41 +2,46 @@
 
 void showMenu();
 void showAllBooks();
+void addNewBooks();
+void sortBooks();
 
-Book myArray[30];
+Book myArray[300];
 int totalBooks = 6;
 
 int main() {
   
 	Book book1;
-	book1.setBookDetails("After", "Anna Todd", "LIN1108", true);
+	book1.setBookDetails("After", "Anna Todd", "LIN1108", true); // Book is available
 	Book book2;
-	book2.setBookDetails("Harry Poter", "J.K Rowling", "DAM1703", false);
+	book2.setBookDetails("Harry Poter", "J.K Rowling", "DAM1703", false); // Book is not available
+	//Book book3;
+	//book3.setBookDetails("The Summer I Turned Pretty", "Jenny Han", "MAM2301", true); // Book is available
 	Book book3;
-	book3.setBookDetails("The Summer I Turned Pretty", "Jenny Han", "MAM2301", true);
-	Book book4;
-	book4.setBookDetails("A Game Of Thrones", "George.R Martin", "PAP0408", true);
-	Book book5;
-	book5.setBookDetails("The Lord Of The Rings", "J.R.R Tolkien", "TIKI1102", false);
-	Book book6;
-	book6.setBookDetails("Bridgerton", "Julia Quinn", "SEGA1708", true);
+	book3.setBookDetails("A Game Of Thrones", "George.R Martin", "PAP0408", true); // Book is available
+	//Book book5;
+	//book5.setBookDetails("The Lord Of The Rings", "J.R.R Tolkien", "TIKI1102", false); // Book is not available
+	//Book book6;
+	//book6.setBookDetails("Bridgerton", "Julia Quinn", "SEGA1708", true); // Book is available
 
-	myArray[0] = book1;
+	myArray[0] = book1; 
 	myArray[1] = book2;
+	//myArray[2] = book3;
 	myArray[2] = book3;
-	myArray[3] = book4;
-	myArray[4] = book5;
-	myArray[5] = book6;
+	//myArray[4] = book5;
+	//myArray[5] = book6;
 
 	int option;
 	while (true) {
-		showMenu();
+		showMenu(); 
 		cout << "Select an option: ";
 		cin >> option;
 		if (option == 1) {
 			showAllBooks();
 		}
 		if (option == 2) {
+			cout << "========================================" << endl;
+			cout << "=          Borrow Book                 =" << endl;
+			cout << "========================================" << endl;
 			string isbnToBorrow = "";
 			while (isbnToBorrow != "0") {
 				cout << "==> Enter the book's ISBN you want to borrow or tap 0 (zero) to exit: ";
@@ -58,6 +63,9 @@ int main() {
 			}
 		}
 		if (option == 3) {
+			cout << "========================================" << endl;
+			cout << "=          Return Book                 =" << endl;
+			cout << "========================================" << endl;
 			string isbnToReturn;
 			cout << "==> Enter the ISBN of the book you want to return or 0 (zero) to exit: ";
 			cin >> isbnToReturn;
@@ -71,6 +79,12 @@ int main() {
 			}
 		}
 		if (option == 4) {
+			addNewBooks();
+		}
+		if (option == 5) {
+			sortBooks();
+		}
+		if (option == 6) {
 			cout << "Bye-Bye thank you for you visit!" << endl;
 			cout << "Hope to see you again!" << endl;
 			break;
@@ -81,20 +95,91 @@ int main() {
 
 void showMenu() {
 	cout << "========================================" << endl;
-	cout << "=                OPTIONS               =" << endl;
+	cout << "=          Library System              =" << endl;
+	cout << "========================================" << endl;
 	cout << "= 1: Show all books                    =" << endl;
 	cout << "= 2: Borrow Books                      =" << endl;
 	cout << "= 3: Return Books                      =" << endl;
-	cout << "= 4: EXIT the program                  =" << endl;
+	cout << "= 4: Add New Books                     =" << endl;
+	cout << "= 5: Sort Books                        =" << endl;
+	cout << "= 6: EXIT the program                  =" << endl;
 	cout << "========================================" << endl;
+	cout << endl;
 }
 
 void showAllBooks() {
+	cout << "========================================" << endl;
+	cout << "=         Display library              =" << endl;
+	cout << "========================================" << endl;
+	int counter = 0;
 	for (int i = 0; i < totalBooks; i++) {
-		cout << "ISBN: " << myArray[i].isbn << ", "
-			<< "Title: " << myArray[i].title << ", "
-			<< "Author: " << myArray[i].author << ", "
-			<< "Available: " << myArray[i].available << endl;
+		if (!myArray[i].isbn.empty() && !myArray[i].title.empty()) {
+			cout << "Title: " << myArray[i].title << ", "
+				<< "Author: " << myArray[i].author << ", "
+				<< "ISBN: " << myArray[i].isbn << ", "
+				<< "Available: " << myArray[i].available << endl;
+				counter++;
+		}
+		
+	}
+	if (counter == 0) {
+		cout << "==> There is No books registered in this library." << endl;
 	}
 	cout << endl;
+}
+
+void addNewBooks() {
+	cout << "========================================" << endl;
+	cout << "=          Add New Books               =" << endl;
+	cout << "========================================" << endl;
+	string title;
+	string author;
+	string isbn;
+	bool available;
+	int index = 0;
+	char answer;
+	bool addAnotherBook = true;
+	while (addAnotherBook) {
+		Book book;
+		cin.ignore(); // Clear the newline character from the input buffer
+		cout << "==> Enter the book's title: ";
+		getline(cin, title);
+		cout << "==> Enter the book's author: ";
+		getline(cin, author);
+		cout << "==> Enter the book's ISBN: ";
+		getline(cin, isbn);
+		/*cout << "==> Is the book available? (1 for Yes, 0 for No): ";
+		cin >> available;*/
+		book.setBookDetails(title, author, isbn, true);
+		myArray[index] = book;
+		cout << "==> Book " << book.title << " added successfully!" << endl;
+		index++;
+		cout << "Do you want to add another book? (Y/N): ";
+		cin >> answer;
+		if (answer == 'N' || answer == 'n') {
+			addAnotherBook = false;
+		} else if (answer != 'Y' && answer != 'y') {
+			cout << "==> Invalid input. Please enter Y or N." << endl;
+			continue; // Ask again
+		}
+	}
+	cout << endl;
+}
+
+void sortBooks() {
+	cout << "========================================" << endl;
+	cout << "=          Sort Books                  =" << endl;
+	cout << "========================================" << endl;
+	// Simple bubble sort based on book titles
+	for (int i = 0; i < totalBooks - 1; i++) {
+		for (int j = i + 1; j < totalBooks; j++) {
+			if (myArray[i].title > myArray[j].title) {
+				Book temp = myArray[i];
+				myArray[i] = myArray[j];
+				myArray[j] = temp;
+			}
+		}
+	}
+	cout << "==> Books sorted successfully!" << endl;
+	showAllBooks();
 }
